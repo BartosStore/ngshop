f3a03b61960f447f16d4a68d148366e5feb63f18
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-04-21 11:47:56 +0200

    Replace hardcoded values with string interpolation (ts values)

--- src/app/administration/administration.component.ts
+++ src/app/administration/administration.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./administration.component.css']
 })
 export class AdministrationComponent implements OnInit {
+  user = "Josef Novak";
+  state = "Active";
+  emailNotification = "on";
+  address = "Kvetinova 240";
+  city = "Jicin"
 
   constructor() { }
 
