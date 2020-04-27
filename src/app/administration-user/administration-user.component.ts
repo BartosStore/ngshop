2096aca9bc6b35c1ac00f08043bc357ea0ed4e2b
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-04-27 11:20:41 +0200

    Add custom filter for autocomplete

--- src/app/administration-user/administration-user.component.ts
+++ src/app/administration-user/administration-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-administration-user',
@@ -18,13 +21,20 @@ export class AdministrationUserComponent implements OnInit {
   ageTickInterval = 100;
   ageValue = 28;
 
-  professions = ['programmer', 'administrator', 'manager'];
+  formControl = new FormControl();
+  professions: string[] = ['programmer', 'administrator', 'manager'];
+  filteredProfessions: Observable<string[]>;
 
   isNotificationsEnabled = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.filteredProfessions = this.formControl.valueChanges.pipe(startWith(''), map(inputValue => this._filterProfessions(inputValue)));
   }
 
+  _filterProfessions(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.professions.filter(profession => profession.toLowerCase().includes(filterValue));
+  }
 }
