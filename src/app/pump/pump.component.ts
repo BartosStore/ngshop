a3b74a72271e804e8222ca8805feefba6ada5ab6
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-04-28 11:34:46 +0200

    Load voting data from file

--- src/app/pump/pump.component.ts
+++ src/app/pump/pump.component.ts
@@ -1,17 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import JsonData from './voters_data.json';
 
 export interface Voter {
   name: string,
   age: number,
   city: string
 }
-
-const VOTERS_DATA: Voter[] = [
-  { name: 'Jan', age: 33, city: 'Praha' },
-  { name: 'Josef', age: 55, city: 'Liberec' },
-  { name: 'Miroslav', age: 44, city: 'Jičín' },
-  { name: 'Vladislav', age: 22, city: 'Praha' },
-];
 
 @Component({
   selector: 'app-pump',
@@ -20,11 +14,10 @@ const VOTERS_DATA: Voter[] = [
 })
 export class PumpComponent implements OnInit {
   dataColumns: string[] = ['index', 'name', 'age', 'city'];
-  dataSource = VOTERS_DATA;
-
-  constructor() { }
+  dataSource: Voter[];
 
   ngOnInit(): void {
+    console.log(JsonData);
+    this.dataSource = JsonData;
   }
-
 }
