a47312a7d15fcc9303e9a301b659a285f1608c3f
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-04-30 13:58:14 +0200

    Display dialog data as table

--- src/app/print-dialog/print-dialog.component.ts
+++ src/app/print-dialog/print-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Voter } from '../pump/pump.component';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-print-dialog',
@@ -8,7 +9,13 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./print-dialog.component.css']
 })
 export class PrintDialogComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) public voters: Voter[]) { }
+  dataColumns: string[] = ['name', 'age', 'city'];
+  votersDataSource: MatTableDataSource<Voter>;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public voters: Voter[]) {
+    console.log(voters);
+    this.votersDataSource = new MatTableDataSource<Voter>(voters.slice());
+  }
 
   ngOnInit(): void {
   }
