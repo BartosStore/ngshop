e8140837261a22f5342599edf4fa20e0085a361c
Miroslav Bartoš <bartos.m@hanaboso.com>
2020-05-06 15:58:26 +0200

    Create user service

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { IUser } from '../model/user';
+
+@Injectable()
+export class UserService {
+  usersUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private _http: HttpClient) {}
+
+  fetchUsers(): Observable<IUser[]> {
+    console.log('fetchUsers');
+    return this._http.get<IUser[]>(this.usersUrl);
+  }
+}
